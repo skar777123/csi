ee6f71227297f43e2650c2c382cab4b501e811ae
skar777123 <sanskargaikwad206@gmail.com>
2024-12-08 19:01:23 +0530

    commitinf 1st

---
+++ components/Backround.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import { BackgroundBeams } from "./ui/background-beams";
+import Image from "next/image";
+
+export function Background() {
+  return (
+    <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          Computer Society of India
+        </h1>
+        <h6 className="relative mt-10 z-10 text-lg md:text-3xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          B. K. Birla College Kalyan Student Branch
+        </h6>
+        {/* <div className="flex flex-row justify-center mt-10">
+          <Image src={"/csi.jpeg"} alt="csi.jpeg" width={100} height={100} className="rounded-bl-3xl rounded-tr-3xl"/>
+        </div> */}
+      </div>
+      <BackgroundBeams />
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +19,15 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 } satisfies Config;
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
